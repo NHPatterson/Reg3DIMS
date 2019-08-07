a075dafe5ebc63ba48d83c78ef865cf4eae60fad
Patterson <nathan.h.patterson@vanderbilt.edu>
2019-08-07 14:28:31 -0500

    Add registration chaining, plots

--- R/reg3DIMS.R
+++ R/reg3DIMS.R
@@ -5,9 +5,13 @@
 #' @param mid_point depth from which registration should proceed
 #' @param flip_position optional, depth at which the image is mirror, length must match length of flip_type
 #' @param flip_type optional, 1 (horizontral) or 2(vertical), length must match length of flip_position
+#' @param ... arguments passed to niftyreg.linear
 #' @return named list of NiftyReg transformations per 2D section
 
-reg3DIMS <- function(reg_images, mid_point = 5,flip_positions = c(), flip_type = c()){
+reg3DIMS <- function(reg_images, reg_seq = c('rigid','nonlinear'), mid_point = 5,flip_positions = c(), flip_type = c(), reg_seq_args = list(
+  rigid = list(nLevels=5, maxIterations = 10, interpolation = 0),
+  nonlinear = list(nBins = 128,finalSpacing = c(20,20,20), spacingUnit=c('voxel'),interpolation = 0)),
+  plot = T){
   
   if(length(flip_positions) != length(flip_type)){
     stop('flip_positions is not equal to the number of flip_types \n
@@ -30,18 +34,18 @@ reg3DIMS <- function(reg_images, mid_point = 5,flip_positions = c(), flip_type =
   for(idx in backward){
     if(idx == max(backward)){
       dfs[[idx]] <- data.frame(idx = idx, source_im_tformed = F, source_im = im_seq[idx], 
-                 target_im_tformed = F, target_im = im_seq[idx+1], stringsAsFactors = F)
+                               target_im_tformed = F, target_im = im_seq[idx+1], stringsAsFactors = F)
       
     }else{
       dfs[[idx]] <- data.frame(idx = idx, source_im_tformed = F, source_im = im_seq[idx], 
-                 target_im_tformed = T, target_im = im_seq[idx+1], stringsAsFactors = F)
+                               target_im_tformed = T, target_im = im_seq[idx+1], stringsAsFactors = F)
       
     }
   }
   
   tform_df_backward <- do.call(rbind, dfs)
   tform_df_backward <- tform_df_backward[order(-tform_df_backward$idx),]
-    
+  
   dfs <- list()
   for(idx in forward){
     if(idx == min(forward)){
@@ -69,7 +73,6 @@ reg3DIMS <- function(reg_images, mid_point = 5,flip_positions = c(), flip_type =
   tform_df_full$flip_type[flip_idx] <- flip_type
   
   tforms <- list()
-  
   for(row in seq_len(nrow(tform_df_full))){
     source_im <- tform_df_full[row,'source_im']
     target_im <- tform_df_full[row,'target_im']
@@ -92,7 +95,7 @@ reg3DIMS <- function(reg_images, mid_point = 5,flip_positions = c(), flip_type =
         f_type <- 'vertically'
         
       }
-
+      
       reg_images[[source_im]] <-applyTransform(aff,reg_images[[source_im]] ,0)
       tforms[[source_im]]$init <- aff
       
@@ -101,209 +104,51 @@ reg3DIMS <- function(reg_images, mid_point = 5,flip_positions = c(), flip_type =
       
     }
     cat('Registration of ', source_im, ' to ', target_im, '\n')
+    #add control over registration sequencing
+    for(scope in reg_seq){
+      
+      reg_seq_args[[scope]]$source<- reg_images[[source_im]]
+      reg_seq_args[[scope]]$target<- reg_images[[target_im]]
+      reg_seq_args[[scope]]$scope <- scope
+      
+      #initialize with previous transformations
+      #weird issue here, but ignore for now
+      if(length(reg_seq) > 1 & which(reg_seq == scope) > 1){
+        if(scope %in% c('rigid','affine')){
+          reg_seq_args[[scope]]$init <- forward(tform)
+          
+        }else if(scope == 'nonlinear'){
+          reg_seq_args[[scope]]$init <- forward(tform)
+          
+        }
+      }
+      tform <- do.call(niftyreg, reg_seq_args[[scope]])
+    }
     
-    
-    rigid <- niftyreg(reg_images[[source_im]],
-                      reg_images[[target_im]],
-                      scope=c("rigid"),
-                      nLevels = 5L,maxIterations = 10,
-                      verbose=F, interpolation=0)
-    
-    tforms[[source_im]]$nifty <- niftyreg(reg_images[[source_im]],
-                                  reg_images[[target_im]],
-                                  init=forward(rigid),
-                                  scope=c("affine"),nLevels = 3,
-                                  maxIterations = 10,verbose=F,
-                                  interpolation=0)
+    tforms[[source_im]]$nifty <- tform
     
     tform_im_mat <- tforms[[source_im]]$nifty$image[1:nrow(tforms[[source_im]]$nifty$image),
-                                         1:ncol(tforms[[source_im]]$nifty$image)]
+                                                    1:ncol(tforms[[source_im]]$nifty$image)]
     
     reg_images[[source_im]] <- tform_im_mat
-    
+
     bg <- EBImage::normalize(reg_images[[target_im]])
     
-    overlay <- paintObjects(bg, 
-                            toRGB(EBImage::normalize(tform_im_mat)), 
-                            col=c("green"), opac=c(0.3), thick=F)
+    overlay <- rgbImage(red = bg, green = EBImage::normalize(tform_im_mat))
     
-    display(overlay,method='raster')
+    if(plot == T){
+      display(overlay, method='raster')
+      
+      text(x = 20, y = 20, label = 
+             paste0(source_im, ' to ', target_im), adj = c(0,1), col = "white", cex = 1)
+    }
+
     
+    tforms[[source_im]]$overlay <- overlay
   }
   
   tforms[[im_seq[mid_point]]]$init <- 'mid_point'
-  
+  tforms <- tforms[order(names(tforms))]
   return(tforms) 
-}
-  
-  # 
-  # ri_backward <- list()
-  # ri_forward <- list()
-  # 
-  # # if(mid_point %in% flip_positions){
-  # #   ft <- flip_df$flip_type[which(flip_df$flip_pos == mid_point)]
-  # #   
-  # #   if(ft == 1){
-  # #     aff <- buildAffine(translation = c(0, 0, 0), scales = c(1, -1, 1),
-  # #                        skews = c(0, 0,0), angles = 0,
-  # #                        source = reg_images[[position-1]],
-  # #                        target = reg_images[[position]],
-  # #                        anchor =  "centre")
-  # #   }else{
-  # #     aff <- buildAffine(translation = c(0, 0, 0), scales = c(-1, 1, 1),
-  # #                        skews = c(0, 0,0), angles = 0,
-  # #                        source = reg_images[[position-1]],
-  # #                        target = reg_images[[position]],
-  # #                        anchor =  "centre")
-  # #   }
-  # #   reg_images[[mid_point]] <-applyTransform(aff,reg_images[[mid_point]] ,0)
-  # #   
-  # #   ri_backward[[mid_point]] <- T
-  # #   ri_backward[[mid_point]]$init_tform <- aff
-  # #   
-  # # }
-  # 
-  # for(position in backward){
-  # 
-  #   if(position == max(backward)){
-  # 
-  #     cat('target: ', position, '\n')
-  #     cat('source: ', position - 1, '\n')
-  #     
-  #     ri_backward[[position]] <- reg_images[[mid_point]]
-  #     rigid <- niftyreg(reg_images[[position-1]],
-  #                       reg_images[[position]],
-  #                       scope=c("rigid"),
-  #                       nLevels = 5L,maxIterations = 100,
-  #                       verbose=F, interpolation=0)
-  #     
-  #     ri_backward[[position - 1]] <- niftyreg(reg_images[[position-1]],
-  #                                             reg_images[[position]],
-  #                                             init=forward(rigid),
-  #                                             scope=c("affine"),nLevels = 3,
-  #                                             maxIterations = 20,verbose=F,
-  #                                             interpolation=0)
-  #     
-  #   } else {
-  #     if(position %in% flip_positions){
-  #       
-  #       ft <- flip_df$flip_type[which(flip_df$flip_pos == position)]
-  #       
-  #       if(ft == 1){
-  #         aff <- buildAffine(translation = c(0, 0, 0), scales = c(1, -1, 1),
-  #                            skews = c(0, 0,0), angles = 0,
-  #                            source = reg_images[[position+1]],
-  #                            target = reg_images[[position]],
-  #                            anchor =  "centre")
-  #       }else{
-  #         aff <- buildAffine(translation = c(0, 0, 0), scales = c(-1, 1, 1),
-  #                            skews = c(0, 0,0), angles = 0,
-  #                            source = reg_images[[position+1]],
-  #                            target = reg_images[[position]],
-  #                            anchor =  "centre")
-  #       }
-  #       
-  #       
-  #       
-  #       out <-applyTransform(aff,reg_images[[position+1]] ,0)
-  #       
-  #       rigid <- niftyreg(out, ri_backward[[position]]$image, scope=c("rigid"),nLevels = 5L,maxIterations = 100,verbose=F,interpolation=0)
-  #       
-  #       ri_backward[[position + 1]] <- niftyreg(out, ri_backward[[position]]$image, scope=c("affine"),init=forward(rigid),nLevels = 3L,maxIterations = 20,verbose=F,interpolation=0)
-  #       
-  #       ri_backward[[position + 1]]$init_tform <- aff
-  #       
-  #       
-  #     }else{
-  #       cat('target: ', position, '\n')
-  #       cat('source: ', position - 1, '\n')
-  #       
-  #       rigid <- niftyreg(reg_images[[position-1]],
-  #                         ri_backward[[position]]$image,
-  #                         scope=c("rigid"),nLevels = 5L,
-  #                         maxIterations = 100,verbose=F,
-  #                         interpolation=0)
-  #       
-  #       ri_backward[[position - 1]] <- niftyreg(reg_images[[position-1]],
-  #                                               ri_backward[[position]]$image,
-  #                                               init=forward(rigid),
-  #                                               scope=c("affine"),
-  #                                               nLevels = 3L, maxIterations = 20,
-  #                                               verbose=F, interpolation=0)
-  #     }
-  #     
-  #   }
-  #   
-  #   
-  # }
-  # 
-  # for(position in forward){
-  #   
-  #   if(position == min(forward)){
-  #     cat('target: ', position, '\n')
-  #     cat('source: ', position + 1, '\n')
-  #     
-  #     
-  #     
-  #     ri_forward[[position]] <- reg_images[[mid_point]]
-  #     
-  #     rigid <- niftyreg(reg_images[[position+1]], reg_images[[position]] ,scope=c("rigid"),nLevels = 5L,maxIterations = 100,verbose=F,interpolation=0)
-  #     
-  #     ri_forward[[position + 1]] <- niftyreg(reg_images[[position+1]], reg_images[[position]] ,scope=c("affine"),init=forward(rigid),nLevels = 3L,maxIterations = 20,verbose=F,interpolation=0)
-  #   } else {
-  #     
-  #     if(position %in% flip_positions){
-  #       
-  #       ft <- flip_df$flip_type[which(flip_df$flip_pos == position)]
-  #       
-  #       if(ft == 1){
-  #         
-  #         aff <- buildAffine(translation = c(0, 0, 0), scales = c(1, -1, 1), 
-  #                            skews = c(0, 0,0), angles = 0, 
-  #                            source = reg_images[[position+1]],
-  #                            target = reg_images[[position]], 
-  #                            anchor =  "centre")
-  #       }else{
-  #         
-  #         aff <- buildAffine(translation = c(0, 0, 0), scales = c(-1, 1, 1), 
-  #                            skews = c(0, 0,0), angles = 0, 
-  #                            source = reg_images[[position+1]],
-  #                            target = reg_images[[position]], 
-  #                            anchor =  "centre")
-  #       }
-  #       
-  #       
-  #       out <-applyTransform(aff,reg_images[[position+1]] ,0)
-  #       rigid <- niftyreg(out, ri_forward[[position]]$image, scope=c("rigid"),nLevels = 5L,maxIterations = 100,verbose=F,interpolation=0)
-  #       
-  #       ri_forward[[position + 1]] <- niftyreg(out, ri_forward[[position]]$image, scope=c("affine"),init=forward(rigid),nLevels = 3L,maxIterations = 20,verbose=F,interpolation=0)
-  #       
-  #       ri_forward[[position + 1]]$init_tform <- aff
-  #       
-  #       
-  #       
-  #     }else{
-  #       cat('target: ', position, '\n')
-  #       cat('source: ', position + 1, '\n')
-  #       
-  #       rigid <- niftyreg(reg_images[[position+1]], ri_forward[[position]]$image, scope=c("rigid"),nLevels = 5L,maxIterations = 100,verbose=F,interpolation=0)
-  #       
-  #       ri_forward[[position + 1]] <- niftyreg(reg_images[[position+1]], ri_forward[[position]]$image, scope=c("affine"),init=forward(rigid),nLevels = 3L,maxIterations = 20,verbose=F,interpolation=0)
-  #     }
-  #     
-  #     
-  #   }
-  #   
-  #   
-  # }
-  # tform_list <- c(ri_backward[1:mid_point], ri_forward[mid_point+1:length(ri_forward)])
-  # 
-  # tform_list[sapply(tform_list, is.null)] <- NULL
-  # 
-  # tform_list[mid_point] <- 'Untransformed'
-  # 
-  # names(tform_list) <- levels(pd_all$sample)
-  # 
-  # return(tform_list)
-  #}
+  }
 
